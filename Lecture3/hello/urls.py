fc2cec097f98e9569e501e1756c609eab82b261e
Brandon Fazal <jbtfazal@gmail.com>
2022-10-30 21:30:39 -0500

    finished 40 mins

--- Lecture3/hello/urls.py
+++ Lecture3/hello/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
     #when visitor visits empty url, show the index path
+    path("brandon", views.brandon, name="brandon"),
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
+    #this route can be any string with variable name name. call greet function
+    #pass in variable name as parameter
 ]
--- Lecture3/hello/views.py
+++ Lecture3/hello/views.py
@@ -5,4 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #request represents user HTTP request
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+    #renders a whole template, not just a string
+
+def brandon(request):
+    return HttpResponse("Hello, Brandon!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
+#takes http request and name parameter that allows you to specify that person
